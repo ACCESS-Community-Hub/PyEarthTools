272d471d4a38b5bfea7035641f8c61353399b0f7
Harrison Cook <harrison.cook@bom.gov.au>
2023-04-17 13:47:09 +1000

    Update TransformOperation

--- dset/training/data/operations/__init__.py
+++ dset/training/data/operations/__init__.py
@@ -1,3 +1,4 @@
 
-from dset.training.data.operations import reshape, transforms, values, filters
+from dset.training.data.operations import reshape, values, filters
 from .patch import PatchingDataIndex
+from .transforms import TransformOperation
--- dset/training/data/operations/transforms.py
+++ dset/training/data/operations/transforms.py
@@ -11,53 +11,10 @@ from dset.training.data.templates import (
     DataOperation,
     SequentialIterator,
 )
-
-from dset.training.data.utils import get_callable, get_class
-
-def get_transforms(sources: dict, order : list = None):
-    indexes = []
-    
-    order = order or list(sources.keys())
-
-    for transform in order:
-        kwargs = sources[transform]
-        data_transform = None
-
-        transform = re.sub(r'\[[0-9]*\]', '', transform)
-
-        try:
-            data_transform = get_class(dset.data.transform, transform)
-        except:
-            pass
-        
-        if not data_transform:
-            for alterations in ["__main__.", "","dset.data.transform", "dset.data."]:
-                try:
-                    data_transform = get_callable(alterations + transform)
-                except (ModuleNotFoundError, ImportError, AttributeError, ValueError):
-                    pass
-                if data_transform:
-                    break
-                
-        if not data_transform:
-            raise ValueError(f"Unable to load {transform!r}")
-
-        if not callable(data_transform):
-            if hasattr(data_transform, transform.split(".")[-1]):
-                data_transform = getattr(data_transform, transform.split(".")[-1])
-            else:
-                raise TypeError(
-                    f"{transform!r} is a {type(data_transform)}, must be callable"
-                )
-        try:
-            indexes.append(data_transform(**kwargs))
-        except Exception as e:
-            raise RuntimeError(f"Initialising {transform} raised {e}")
-    return dset.data.transform.TransformCollection(indexes)
-            
+from dset.training.data.utils import get_callable, get_class, get_transforms
 
 @SequentialIterator
-class Transforms(DataOperation):
+class TransformOperation(DataOperation):
     """
     Apply dset.data transforms to data
     """
