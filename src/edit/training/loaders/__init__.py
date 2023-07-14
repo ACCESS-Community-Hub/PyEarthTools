6819bab0cc84f1fbb07236e090f1b786ea196dd6
Harrison Cook <harrison.cook@bom.gov.au>
2023-07-14 15:22:01 +1000

    Fix pipeline reference

--- src/edit/training/loaders/__init__.py
+++ src/edit/training/loaders/__init__.py
@@ -1,13 +1,13 @@
 """
-Collection of Dataloaders to use at the end of the Pipeline
+Collection of Dataloaders to use at the end of the training
 
 | Name                | Description |
 | ------------------- | ----------- |
-| [PytorchIterable][edit.pipeline.loaders.pytorch]     | Basic PyTorch IterableDataset |
-| [CustomLoader][edit.pipeline.loaders.custom]     | Basic Custom DataLoader to batch data |
+| [PytorchIterable][edit.training.loaders.pytorch]     | Basic PyTorch IterableDataset |
+| [CustomLoader][edit.training.loaders.custom]     | Basic Custom DataLoader to batch data |
 """
 
-from edit.pipeline.loaders.pytorch import PytorchIterable
-from edit.pipeline.loaders.custom import CustomLoader
+from edit.training.loaders.pytorch import PytorchIterable
+from edit.training.loaders.custom import CustomLoader
 
 # from .dali import DALILoader
