d7cf4a30411c8ee354c0271ff1661d18065204f8
Harrison Cook <harrison.cook@bom.gov.au>
2024-07-15 14:31:48 +1000

    Rename index to dataindex

--- src/edit/training/__init__.py
+++ src/edit/training/__init__.py
@@ -17,7 +17,7 @@ and allow rapid distributed training of Machine Learning Models.
 from edit.training import data, wrapper, manage
 
 from edit.training.wrapper import *  # type: ignore # noqa: F403
-from edit.training.index import MLDataIndex
+from edit.training.dataindex import MLDataIndex
 
 # try:
 #     from edit.training import modules
