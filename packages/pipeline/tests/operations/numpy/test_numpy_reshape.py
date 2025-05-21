bab31ada7699264dda2252f4a231f3c6868b9821
Gemma Mason <masonge@NIWA-1017848.local>
2025-05-21 15:02:48 +1200

    test swapaxis

--- packages/pipeline/tests/operations/numpy/test_numpy_reshape.py
+++ packages/pipeline/tests/operations/numpy/test_numpy_reshape.py
@@ -130,6 +130,11 @@ def test_Flatten_all_dims():
     assert output.shape == (6*7*5*2,)
     assert f3.undo_func(output).shape == (6, 7, 5, 2), "Undo Flatten all dimensions."
 
+def test_Flatten_with_shape_attempt():
+    f = reshape.Flatten(shape_attempt = (2, 1, 1, 1))
+    undo_data = np.zeros((2))
+    assert f.undo_func(undo_data).shape == (2, 1, 1, 1)
+
 def test_SwapAxis():
     s = reshape.SwapAxis(1, 3)
     random_array = np.random.randn(5, 7, 8, 2)
