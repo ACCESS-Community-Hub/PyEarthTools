ae4bf05d8804ddabbbf75323d27a908d1f0f8192
Gemma Mason <masonge@NIWA-1017848.local>
2025-05-21 14:50:23 +1200

    test swapaxis

--- packages/pipeline/tests/operations/numpy/test_numpy_reshape.py
+++ packages/pipeline/tests/operations/numpy/test_numpy_reshape.py
@@ -95,8 +95,17 @@ def test_Flattener():
     f = reshape.Flattener()
     random_array = np.random.randn(4, 3, 5)
     output = f.apply(random_array)
-    undo_output = f.undo_func(output)
+    undo_output = f.undo(output)
     assert len(output.shape) == 1, "Flattener produces a 1D array."
+    assert np.all(undo_output == random_array), "Flattener can undo itself."
+
+def test_Flattener_1_dim():
+    f2 = reshape.Flattener(flatten_dims=1)
+    random_array = np.random.randn(4, 3, 5)
+    output = f2.apply(random_array)
+    undo_output = f2.undo(output) # Check that the undo still works.
+    assert np.all(output == random_array), "Flatten 1 dimension does nothing."
+    assert np.all(undo_output == random_array), "Undo Flatten 1 dimension."
 
 def test_Flatten():
     f1 = reshape.Flatten(flatten_dims=2)
@@ -106,14 +115,26 @@ def test_Flatten():
     assert output.shape == (4, 3*5), "Flatten acts on the last few dimensions."
     assert np.all(undo_output == random_array), "Flatten can undo itself."
 
+def test_Flatten_1_dim():
     f2 = reshape.Flatten(flatten_dims=1)
     random_array = np.random.randn(4, 3, 5)
     output = f2.apply_func(random_array)
+    undo_output = f2.undo_func(output) # Check that the undo still works.
     assert np.all(output == random_array), "Flatten 1 dimension does nothing."
+    assert np.all(undo_output == random_array), "Undo Flatten 1 dimension."
 
+def test_Flatten_all_dims():
     f3 = reshape.Flatten()
     random_array3 = np.random.randn(6, 7, 5, 2)
     output = f3.apply_func(random_array3)
+    assert output.shape == (6*7*5*2,)
     assert f3.undo_func(output).shape == (6, 7, 5, 2), "Undo Flatten all dimensions."
 
+def test_SwapAxis():
+    s = reshape.SwapAxis(1, 3)
+    random_array = np.random.randn(5, 7, 8, 2)
+    output = s.apply_func(random_array)
+    assert output.shape == (5, 2, 8, 7), "Swap axes 1 and 3"
+    undo_output = s.undo_func(output)
+    assert np.all(undo_output == random_array), "Undo axis swap."
 
