657993f7d817156a80e1dad7f6cbc50b8b263872
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-06-01 15:08:43 +1000

    Remove two files created for debugging purposes

--- notebooks/debug.py
+++
@@ -1,9 +0,0 @@
-import pyearthtools.data as petdata
-import pyearthtools.pipeline as petpipe
-import site_archive_nci
-
-cmip5_model1 = petdata.archive.CMIP5(
-    institutions="BCC", scenarios=["rcp60"], models=["bcc-csm1-1"], interval="mon", variables="tas"
-)
-
-ds_cmip_2010 = cmip5_model1["2010-01"]  # Query data along primary dimension
