c6660c28fe36556e738a7870d94e7fcb195f67b2
Edward Yang <edward_yang_125@hotmail.com>
2025-11-12 09:29:34 +1100

    add TemporalRetrieval test and move TemporalWindow test

--- packages/pipeline/tests/pipeline/test_idx_mod.py
+++ packages/pipeline/tests/pipeline/test_idx_mod.py
@@ -19,9 +19,16 @@ import pytest
 
 import pyearthtools.utils
 
-from pyearthtools.pipeline import Pipeline, modifications
-from pyearthtools.pipeline import Operation
-from pyearthtools.data import Index
+from pyearthtools.pipeline import Pipeline, Operation
+from pyearthtools.data import Index, Petdt
+from pyearthtools.pipeline.modifications import (
+    IdxModifier,
+    IdxOverride,
+    TimeIdxModifier,
+    TemporalWindow,
+    TemporalRetrieval,
+)
+from pyearthtools.data.time import TimeDelta
 
 
 class FakeIndex(Index):
@@ -58,32 +65,32 @@ def test_multiplication_undo():
 
 
 def test_IdxModifier_basic():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier((0,)))
+    pipe = Pipeline(FakeIndex(), IdxModifier((0,)))
     assert pipe[0] == (0,)
 
 
 def test_IdxModifier_basic_no_tuple():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier(0))
+    pipe = Pipeline(FakeIndex(), IdxModifier(0))
     assert pipe[0] == 0
 
 
 def test_IdxModifier_two_samples():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier((0, 1)))
+    pipe = Pipeline(FakeIndex(), IdxModifier((0, 1)))
     assert pipe[0] == (0, 1)
 
 
 def test_IdxModifier_nested():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier((0, (1, 2))))
+    pipe = Pipeline(FakeIndex(), IdxModifier((0, (1, 2))))
     assert pipe[0] == (0, (1, 2))
 
 
 def test_IdxModifier_nested_double():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier((0, (1, (2, 3)))))
+    pipe = Pipeline(FakeIndex(), IdxModifier((0, (1, (2, 3)))))
     assert pipe[0] == (0, (1, (2, 3)))
 
 
 def test_IdxModifier_nested_merge():
-    pipe = Pipeline(FakeIndex(), modifications.IdxModifier((0, (1, 2)), merge=True, merge_function=sum))
+    pipe = Pipeline(FakeIndex(), IdxModifier((0, (1, 2)), merge=True, merge_function=sum))
     assert pipe[0] == (0, 3)
 
 
@@ -99,7 +106,7 @@ def test_IdxModifier_nested_merge():
 def test_IdxModifier_merge_depth(depth, result):
     pipe = Pipeline(
         FakeIndex(),
-        modifications.IdxModifier((1, (2, (3, 4))), merge=depth, merge_function=sum),
+        IdxModifier((1, (2, (3, 4))), merge=depth, merge_function=sum),
     )
     assert pipe[0] == result
 
@@ -107,7 +114,7 @@ def test_IdxModifier_merge_depth(depth, result):
 def test_IdxModifier_unmergeable():
     pipe = Pipeline(
         FakeIndex("test"),  # type: ignore
-        modifications.IdxModifier(("t", "a"), merge=True, merge_function=sum),
+        IdxModifier(("t", "a"), merge=True, merge_function=sum),
     )
     with pytest.raises(TypeError):
         assert pipe[1] == (1, 5)
@@ -116,8 +123,8 @@ def test_IdxModifier_unmergeable():
 def test_IdxMod_stacked():
     pipe = Pipeline(
         FakeIndex(),
-        modifications.IdxModifier((0, 1)),
-        modifications.IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
     )
     assert pipe[1] == ((1, 2), (2, 3))
 
@@ -125,8 +132,8 @@ def test_IdxMod_stacked():
 def test_IdxMod_stacked_with_mult():
     pipe = Pipeline(
         FakeIndex(),
-        modifications.IdxModifier((0, 1)),
-        modifications.IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
         MultiplicationOperation(2),
     )
     assert pipe[1] == ((2, 4), (4, 6))
@@ -135,7 +142,7 @@ def test_IdxMod_stacked_with_mult():
 def test_IdxMod_with_branch():
     pipe = Pipeline(
         FakeIndex(),
-        modifications.IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
         (
             (MultiplicationOperation(1),),
             (MultiplicationOperation(2),),
@@ -147,7 +154,7 @@ def test_IdxMod_with_branch():
 def test_IdxMod_with_branch_mapping():
     pipe = Pipeline(
         FakeIndex(),
-        modifications.IdxModifier((0, 1)),
+        IdxModifier((0, 1)),
         ((MultiplicationOperation(1),), (MultiplicationOperation(2),), "map"),
     )
     assert pipe[1] == (1, 4)
@@ -157,7 +164,7 @@ def test_IdxMod_with_branch_mapping():
 
 
 def test_IdxOverride_basic():
-    pipe = Pipeline(FakeIndex(), modifications.IdxOverride(0))
+    pipe = Pipeline(FakeIndex(), IdxOverride(0))
     assert pipe[1] == 0
 
 
@@ -167,7 +174,7 @@ def test_IdxOverride_basic():
 def test_TimeIdxModifier_basic():
     import pyearthtools.data
 
-    pipe = Pipeline(FakeIndex(), modifications.TimeIdxModifier("6 hours"))
+    pipe = Pipeline(FakeIndex(), TimeIdxModifier("6 hours"))
     assert pipe[pyearthtools.data.Petdt("2000-01-01T00")] == pyearthtools.data.Petdt("2000-01-01T06")
 
 
@@ -180,8 +187,65 @@ def test_TimeIdxModifier_basic():
 def test_TimeIdxModifier_nested():
     import pyearthtools.data
 
-    pipe = Pipeline(FakeIndex(), modifications.TimeIdxModifier(("6 hours", "12 hours")))
+    pipe = Pipeline(FakeIndex(), TimeIdxModifier(("6 hours", "12 hours")))
     assert pipe[pyearthtools.data.Petdt("2000-01-01T00")] == (
         pyearthtools.data.Petdt("2000-01-01T06"),
         pyearthtools.data.Petdt("2000-01-01T12"),
     )
+
+
+class test_data_accessor(Index):
+    _data = {
+        "2025-01-01": 1,
+        "2025-01-02": 2,
+        "2025-01-03": 3,
+    }
+
+    def get(self, time):
+        return self._data[time]
+
+
+@pytest.mark.parametrize("merge_method", (None, sum))
+def test_temporal_retrieval(merge_method):
+    """Test temporal retrieval."""
+
+    # instantiate temporal retrieval with merge_method - retrieving from two steps behind.
+    temporal_retrieval_step = TemporalRetrieval(-2, merge_function=merge_method, delta_unit="day")
+
+    pipeline = Pipeline(test_data_accessor(), temporal_retrieval_step)
+
+    result = pipeline[Petdt("2025-01-03")]
+    if merge_method is sum:
+        assert result == test_data_accessor._data["2025-01-03"] + test_data_accessor._data["2025-01-01"]
+    elif merge_method is None:
+        assert result == (test_data_accessor._data["2025-01-01"], test_data_accessor._data["2025-01-03"])
+
+
+def test_temporal_retrieval_invalid():
+    """Tests errors when using/instantiating TemporalRetrieval."""
+    with pytest.raises(ValueError):
+        TemporalRetrieval(None)  # index ought to be int or iterable of ints
+    tr = TemporalRetrieval(-1)
+    with pytest.raises(TypeError):
+        tr["a"]  # not convertable to Petdt
+
+
+@pytest.mark.parametrize("merge_method", (None, sum))
+def test_temporal_window(merge_method):
+    """Test temporal window."""
+
+    # instantiate temporal window with merge method
+    temporal_window_step = TemporalWindow(
+        prior_indexes=[-2, -1],
+        posterior_indexes=[0],
+        timedelta=TimeDelta((1, "day")),
+        merge_method=merge_method,
+    )
+
+    # Instantiate pipeline with test data and temporal window
+    pipeline = Pipeline(test_data_accessor(), temporal_window_step)
+    result = pipeline["2025-01-03"]
+    if merge_method is None:
+        assert result == ([1, 2], [3])
+    elif merge_method is sum:
+        assert result == (3, 3)
