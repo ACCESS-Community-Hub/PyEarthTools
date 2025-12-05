01d9ade02b4739325605ac563924dbadee170e11
Edward Yang <edward_yang_125@hotmail.com>
2025-12-05 11:43:46 +1100

    remove not in DropAnyNan and DropAllNan

--- packages/pipeline/src/pyearthtools/pipeline/operations/dask/filters.py
+++ packages/pipeline/src/pyearthtools/pipeline/operations/dask/filters.py
@@ -59,7 +59,7 @@ class DropAnyNan(daskFilter):
             (bool):
                 If sample contains nan's
         """
-        if not bool(da.array(list(da.isnan(sample))).any()):
+        if da.array(list(da.isnan(sample))).any():
             raise PipelineFilterException(sample, "Data contained nan's.")
 
 
@@ -85,7 +85,7 @@ class DropAllNan(daskFilter):
             (bool):
                 If sample contains nan's
         """
-        if not bool(da.array(list(da.isnan(sample))).all()):
+        if da.array(list(da.isnan(sample))).all():
             raise PipelineFilterException(sample, "Data contained all nan's.")
 
 
