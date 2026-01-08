f22c67a840afa95254d7a7b745b12727a7925ef9
Edward Yang <edward_yang_125@hotmail.com>
2026-01-08 16:17:24 +1100

    rename numpy ForceNormalised to Clip
    Clip is more appropriate as there is an equivalent dask/numpy/xarray
    function. Additionally, no normalisation is occuring.

--- packages/pipeline/src/pyearthtools/pipeline/operations/numpy/values.py
+++ packages/pipeline/src/pyearthtools/pipeline/operations/numpy/values.py
@@ -129,9 +129,9 @@ class MaskValue(Operation):
         return self._mask_transform(sample)  # type: ignore
 
 
-class ForceNormalised(Operation):
+class Clip(Operation):
     """
-    Operation to force data within a certain range, by default 0 & 1
+    Operation to force data to be within a certain range, by default 0 & 1
     """
 
     _override_interface = ["Delayed", "Serial"]
@@ -164,10 +164,8 @@ class ForceNormalised(Operation):
 
         self.record_initialisation()
 
-        self._force_min = MaskValue(min_value, "<", min_value) if min_value is not None else None
-        self._force_max = MaskValue(max_value, ">", max_value) if max_value is not None else None
+        self._min_value = min_value
+        self._max_value = max_value
 
     def apply_func(self, sample):
-        for func in (func for func in [self._force_min, self._force_max] if func is not None):
-            sample = func.apply_func(sample)
-        return sample
+        return np.clip(sample, a_min=self._min_value, a_max=self._max_value)
