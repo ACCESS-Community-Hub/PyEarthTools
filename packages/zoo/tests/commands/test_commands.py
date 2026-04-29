dbb7130fea1f2a996a9f80f52831f3c511b6117f
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2026-04-29 22:24:38 +1000

    Linting

--- packages/zoo/tests/commands/test_commands.py
+++ packages/zoo/tests/commands/test_commands.py
@@ -14,18 +14,18 @@ from pyearthtools.zoo.exceptions import ModelException
 def test_models():
 
     with pytest.raises(SystemExit):
-        m = cmd.list_models()
+        _m = cmd.list_models()
 
 
 def test_run_predict():
 
-    ctx = namedtuple('Any', ['args', 'kwargs'])([], [])
-    model = 'nonexistent'
-    time = '2020T010100'
-    output = 'tbc'
-    pipeline_name = 'tbc'
-    data_cache = 'tbc'
-    config_path = 'tbc'
+    ctx = namedtuple("Any", ["args", "kwargs"])([], [])
+    model = "nonexistent"
+    time = "2020T010100"
+    output = "tbc"
+    pipeline_name = "tbc"
+    data_cache = "tbc"
+    config_path = "tbc"
 
     with pytest.raises(ModelException):
         cmd.cmd_run_predict(ctx, model, time, output, pipeline_name, data_cache, config_path)
@@ -33,28 +33,28 @@ def test_run_predict():
 
 def test_interactive():
 
-    ctx = namedtuple('Any', ['args', 'kwargs'])([], [])
-    model = 'nonexistent'
-    time = '2020T010100'
-    output = 'tbc'
-    pipeline_name = 'tbc'
-    data_cache = 'tbc'
-    config_path = 'tbc'    
+    ctx = namedtuple("Any", ["args", "kwargs"])([], [])
+    model = "nonexistent"
+    time = "2020T010100"
+    output = "tbc"
+    pipeline_name = "tbc"
+    data_cache = "tbc"
+    config_path = "tbc"
 
     with pytest.raises(AttributeError):
-        with patch('pyearthtools.zoo.available_models', return_value='fake_model'):
-            cmd.cmd_interactive(ctx, model, time, pipeline_name, output, data_cache, config_path)     
+        with patch("pyearthtools.zoo.available_models", return_value="fake_model"):
+            cmd.cmd_interactive(ctx, model, time, pipeline_name, output, data_cache, config_path)
 
 
 def test_data():
 
-    ctx = namedtuple('Any', ['args', 'kwargs'])([], [])
-    model = 'nonexistent'
-    time = '2020T010100'
-    output = 'tbc'
-    pipeline_name = 'tbc'
-    data_cache = 'tbc'
-    config_path = 'tbc'
+    ctx = namedtuple("Any", ["args", "kwargs"])([], [])
+    model = "nonexistent"
+    time = "2020T010100"
+    _output = "tbc"
+    pipeline_name = "tbc"
+    data_cache = "tbc"
+    config_path = "tbc"
 
     with pytest.raises(ModelException):
         cmd.cmd_data(ctx, model, time, pipeline_name, data_cache, config_path)
